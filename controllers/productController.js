6bb781daa84934310fa58b9ffd1c0d231af063e4
tarekahmed220 <tarek.ahm12@gmail.com>
2024-09-03 15:03:39 +0300

    fix bug

--- controllers/productController.js
+++ controllers/productController.js
@@ -36,60 +36,62 @@ const create1 = catchAsync(async (req, res) => {
       product,
     },
   });
+});
 
+//get product by id
+const getAllProducts = catchAsync(async (req, res, next) => {
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
 
-    //get product by id
-    const getAllProducts=catchAsync(async(req, res,next) => {
-    
-        const page = parseInt(req.query.page) || 1;
-        const limit = parseInt(req.query.limit) || 10;
-    
-        const startIndex = (page - 1) * limit;
-        const total = await Product.countDocuments();
-    
-        const products = await Product.find().sort({createdAt:-1}).skip(startIndex).limit(limit);
-    
-        res.status(200).json({
-            page,
-            limit,
-            total,
-            pages: Math.ceil(total / limit),
-            data: products
-        });
-    });
-const getOneProduct=catchAsync(async(req,res,next)=>{
-    const productId=req.params.id
-    let product =await Product.findById(productId).populate('ratings')
-    if(!product){
-        return next(new AppError("product not found",404))
-    }
-    res.status(200).json({status:"success",data:{product}})
-})
-const deleteProduct=catchAsync(async(req,res,next)=>{
-    const product=await Product.findById(req.params.id)
-    if(!product){
-        return next(new AppError("product not found",404))
-    }
-    const deletedProduct=await Product.findByIdAndDelete(product)
-    res.status(204).json({status:"success",data:null})
-})
+  const startIndex = (page - 1) * limit;
+  const total = await Product.countDocuments();
 
-const updateProduct=catchAsync(async(req,res,next)=>{
-    const productId=req.params.id
-    let product =await Product.findById(productId)
-    if(!product){
-        return next(new AppError("product not found",404))
-    }
-    const result = await cloudinary.v2.uploader.upload(req.file.path);
-    if (req.file) { req.body.photo=result.secure_url;}
-    const updatedOne= await Product.findByIdAndUpdate(
-        req.params.id, req.body,{
-            new: true,
-            runValidators: true
-          });
-    res.status(200).json({status:"success",data:{updatedOne}})
+  const products = await Product.find()
+    .sort({ createdAt: -1 })
+    .skip(startIndex)
+    .limit(limit);
 
-})
+  res.status(200).json({
+    page,
+    limit,
+    total,
+    pages: Math.ceil(total / limit),
+    data: products,
+  });
+});
+const getOneProduct = catchAsync(async (req, res, next) => {
+  const productId = req.params.id;
+  let product = await Product.findById(productId).populate("ratings");
+  if (!product) {
+    return next(new AppError("product not found", 404));
+  }
+  res.status(200).json({ status: "success", data: { product } });
+});
+const deleteProduct = catchAsync(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new AppError("product not found", 404));
+  }
+  const deletedProduct = await Product.findByIdAndDelete(product);
+  res.status(204).json({ status: "success", data: null });
+});
+
+const updateProduct = catchAsync(async (req, res, next) => {
+  const productId = req.params.id;
+  let product = await Product.findById(productId);
+  if (!product) {
+    return next(new AppError("product not found", 404));
+  }
+  const result = await cloudinary.v2.uploader.upload(req.file.path);
+  if (req.file) {
+    req.body.photo = result.secure_url;
+  }
+  const updatedOne = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({ status: "success", data: { updatedOne } });
+});
 
 // get home products
 
@@ -99,16 +101,11 @@ const getHomeProducts = catchAsync(async (req, res, next) => {
   res.status(200).json([{ msg: "success" }, { homeProducts }]);
 });
 
-export{
-    getAllProducts,
-    
-    getOneProduct,
-    deleteProduct,
-    updateProduct,
-    create1,
-     getHomeProducts,
-}
-
-
-  
-
+export {
+  getAllProducts,
+  getOneProduct,
+  deleteProduct,
+  updateProduct,
+  create1,
+  getHomeProducts,
+};

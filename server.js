5041e6dbc8bb0699fb19bdf43b8c69777368966f
ramh22 <s4alpy@gmail.com>
2024-09-03 16:41:58 +0300

    confirm email

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import app from './app.js'
 
-const port = process.env.PORT || 5000
+const port = 5000 //process.env.PORT || 5000
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

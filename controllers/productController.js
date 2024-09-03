7c747b2a3f8b361ad7b85d437e04e1227919da8a
tarekahmed220 <tarek.ahm12@gmail.com>
2024-09-03 14:55:12 +0300

    get home proudct

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,120 +4,122 @@ import { Product } from "../models/productModel.js";
 import { upload } from "../uploads/multer.js";
 import { deleteOne } from "./factory.js";
 import { cloudinary } from "../uploads/cloudinary.js";
-
-
+import { HomeProductsModel } from "../models/homeProductModel.js";
 
 const create1 = catchAsync(async (req, res) => {
-    //allowed nested routes
-    
-    if (!req.body.user) req.body.user = req.user.id;//from protect middleware
-    
-    const {productName,price,description,category} = req.body;
-    
-  
-      //Upload image to cloudinary
-      
-    const result = await cloudinary.v2.uploader.upload(req.file.path);
-      
-    //create order
-    const product = await Product.create({
-      productName,
-      price,
+  //allowed nested routes
 
-      category,
-      description,
-      photo: result.secure_url,
-      cloudinary_id: result.public_id,
-      
-      user:req.user.id,
-   
-    });
-    
-    res.status(201).json({
-      status:"success",
-     data:{
-      product,
-     }
-    });
-    
+  if (!req.body.user) req.body.user = req.user.id; //from protect middleware
+
+  const { productName, price, description, category } = req.body;
+
+  //Upload image to cloudinary
+
+  const result = await cloudinary.v2.uploader.upload(req.file.path);
+
+  //create order
+  const product = await Product.create({
+    productName,
+    price,
+
+    category,
+    description,
+    photo: result.secure_url,
+    cloudinary_id: result.public_id,
+
+    user: req.user.id,
   });
-  
-// create
-const createOneProduct = catchAsync(async (req, res, next) => {
-    const { productName, price,  description ,category} = req.body;
-    // Ensure required fields are present
-    if (!productName || !price || !description||!category) {
-        return next(new AppError('Product name, price, and description are required', 400));
-    }
-    if(!req.file){
-        return next(new AppError('product Photo are required', 500));
-    }
-      
-        const photo= `http://localhost:5000/uploads/${req.file?.filename}`;
-    const newProduct = await Product.create({ productName, price,category, description ,photo});
-    console.log(newProduct)
-    res.status(200).json({
-        msg: "success",
-        data: newProduct,
-        
-    });
 
+  res.status(201).json({
+    status: "success",
+    data: {
+      product,
+    },
+  });
 });
 
-    //get all products
-const getAllProducts=catchAsync(async(req, res,next) => {
+// create
+const createOneProduct = catchAsync(async (req, res, next) => {
+  const { productName, price, description, category } = req.body;
+  // Ensure required fields are present
+  if (!productName || !price || !description || !category) {
+    return next(
+      new AppError("Product name, price, and description are required", 400)
+    );
+  }
+  if (!req.file) {
+    return next(new AppError("product Photo are required", 500));
+  }
 
- 
-      
-        const products = await Product.find().sort({createdAt:-1})
-    
-  
+  const photo = `http://localhost:5000/uploads/${req.file?.filename}`;
+  const newProduct = await Product.create({
+    productName,
+    price,
+    category,
+    description,
+    photo,
+  });
+  console.log(newProduct);
+  res.status(200).json({
+    msg: "success",
+    data: newProduct,
+  });
+});
 
-       res.status(200).json([{msg:"success"},{products}])
-    })
-    //get product by id
-const getOneProduct=catchAsync(async(req,res,next)=>{
-    const productId=req.params.id
-    let product =await Product.findById(productId).populate('ratings')
-    if(!product){
-        return next(new AppError("product not found",404))
-    }
-    res.status(200).json({status:"success",data:{product}})
-})
-const deleteProduct=catchAsync(async(req,res,next)=>{
-    const product=await Product.findById(req.params.id)
-    if(!product){
-        return next(new AppError("product not found",404))
-    }
-    const deletedProduct=await Product.findByIdAndDelete(product)
-    res.status(204).json({status:"success",data:null})
-})
+//get all products
+const getAllProducts = catchAsync(async (req, res, next) => {
+  const products = await Product.find().sort({ createdAt: -1 });
 
-const updateProduct=catchAsync(async(req,res,next)=>{
-    const productId=req.params.id
-    let product =await Product.findById(productId)
-    if(!product){
-        return next(new AppError("product not found",404))
-    }
-    if (req.file) { req.body.photo=`http://localhost:5000/uploads/${req.file?.filename}`;}
-    const updatedOne= await Product.findByIdAndUpdate(
-        req.params.id, req.body,{
-            new: true,
-            runValidators: true
-          });
-    res.status(200).json({status:"success",data:{updatedOne}})
+  res.status(200).json([{ msg: "success" }, { products }]);
+});
+//get product by id
+const getOneProduct = catchAsync(async (req, res, next) => {
+  const productId = req.params.id;
+  let product = await Product.findById(productId).populate("ratings");
+  if (!product) {
+    return next(new AppError("product not found", 404));
+  }
+  res.status(200).json({ status: "success", data: { product } });
+});
+const deleteProduct = catchAsync(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new AppError("product not found", 404));
+  }
+  const deletedProduct = await Product.findByIdAndDelete(product);
+  res.status(204).json({ status: "success", data: null });
+});
 
-})
+const updateProduct = catchAsync(async (req, res, next) => {
+  const productId = req.params.id;
+  let product = await Product.findById(productId);
+  if (!product) {
+    return next(new AppError("product not found", 404));
+  }
+  if (req.file) {
+    req.body.photo = `http://localhost:5000/uploads/${req.file?.filename}`;
+  }
+  const updatedOne = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({ status: "success", data: { updatedOne } });
+});
 
-export{
-    getAllProducts,
-    createOneProduct,
-    getOneProduct,
-    deleteProduct,
-    updateProduct,
-    create1
-    
-}
+// get home products
 
+const getHomeProducts = catchAsync(async (req, res, next) => {
+  const homeProducts = await HomeProductsModel.find().sort({ createdAt: -1 });
 
-  
+  res.status(200).json([{ msg: "success" }, { homeProducts }]);
+});
+
+export {
+  getAllProducts,
+  createOneProduct,
+  getOneProduct,
+  deleteProduct,
+  updateProduct,
+  create1,
+  getHomeProducts,
+};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,41 +1,35 @@
-
 import express from "express";
-import  verifyToken from "../middlewares/verifyToken.js";
-import roleCheck from"../middlewares/roleCheck.js"
+import verifyToken from "../middlewares/verifyToken.js";
+import roleCheck from "../middlewares/roleCheck.js";
 import { Rating } from "../models/ratingModel.js";
 import { Product } from "../models/productModel.js";
- import {upload} from "../uploads/multer.js"
-import path from 'path'
+import { upload } from "../uploads/multer.js";
+import path from "path";
 import {
-    
-    getAllProducts,
-    createOneProduct,
-    deleteProduct,
-    getOneProduct,
-    updateProduct,
-    create1
-  } from "../controllers/productController.js"
+  getAllProducts,
+  getHomeProducts,
+  createOneProduct,
+  deleteProduct,
+  getOneProduct,
+  updateProduct,
+  create1,
+} from "../controllers/productController.js";
 import { createOneRate } from "../controllers/ratingController.js";
-const router=express.Router()
+const router = express.Router();
 
-
-
-router.route('/cloud').post( verifyToken,upload.single('photo'), create1)
-    // router.route('/').post(upload.single('photo'), createOneProduct)
-     router.route('/').get(getAllProducts)
-
-    router
-    .route('/:id')
-    .delete( deleteProduct)
-    .get(getOneProduct)
-    .patch(upload.single("photo"),updateProduct)
-
+router.route("/cloud").post(verifyToken, upload.single("photo"), create1);
+// router.route('/').post(upload.single('photo'), createOneProduct)
+router.route("/").get(getAllProducts);
+router.route("/homeproducts").get(getHomeProducts);
 
 router
-.route('/:productId/ratings')
-.post(
-    verifyToken,
-    roleCheck('client'),
-    createOneRate)
+  .route("/:id")
+  .delete(deleteProduct)
+  .get(getOneProduct)
+  .patch(upload.single("photo"), updateProduct);
+
+router
+  .route("/:productId/ratings")
+  .post(verifyToken, roleCheck("client"), createOneRate);
 export default router;
 //products/:productId/rating

7047e8e40bf393c314b6a232c8b3e3f90b260efd
AhmedTarek22 <ahmedtarekabdo22@gmail.com>
2024-09-04 12:05:34 +0300

    add cart api

---
+++ modules/cart/cart.routes.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+import {
+  addToCart,
+  removeFromCart,
+  updateCart,
+  getCartCount,
+  getCartItems,
+} from "./cart.controller.js";
+import verifyToken from "../../middlewares/verifyToken.js";
+
+const cartRoutes = express.Router();
+
+cartRoutes.get("/getcartitems", verifyToken, getCartItems);
+cartRoutes.post("/addtocart", verifyToken, addToCart);
+cartRoutes.get("/cart-count", verifyToken, getCartCount);
+cartRoutes.put("/updatecart", verifyToken, updateCart);
+cartRoutes.delete("/removefromcart/:productId", verifyToken, removeFromCart);
+
+export default cartRoutes;

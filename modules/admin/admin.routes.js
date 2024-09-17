f8977dfbae14dda7fe3d0287cb426d7ac8308bcc
tarekahmed220 <tarek.ahm12@gmail.com>
2024-09-17 08:38:36 +0300

    auth

--- modules/admin/admin.routes.js
+++ modules/admin/admin.routes.js
@@ -3,12 +3,24 @@ import {
   getAllClients,
   getAllWorkshops,
   deleteClient,
+  updateWorkshop,
+  getWorkshopRequests,
+  acceptanceState,
 } from "./admin.controller.js";
+import verifyToken from "../../middlewares/verifyToken.js";
+import roleCheck from "../../middlewares/roleCheck.js";
 const adminRoutes = express.Router();
 
 adminRoutes.get("/allclients", getAllClients);
 adminRoutes.get("/allworkshops", getAllWorkshops);
-
 adminRoutes.delete("/deleteclient/:email", deleteClient);
+adminRoutes.put(
+  "/updateworkshop",
+  verifyToken,
+  roleCheck("workshop"),
+  updateWorkshop
+);
+adminRoutes.get("/workshoprequests", getWorkshopRequests);
+adminRoutes.put("/acceptancestate/:email", acceptanceState);
 
 export default adminRoutes;
